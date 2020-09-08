47c77bb61026f6e04d6f3f57585acf101616aa68
YongjinKim <dydwls0669@gmail.com>
2020-09-08 16:05:52 +0900

    variable

---
+++ variable.py
@@ -0,0 +1,40 @@
+print(5)
+print(-5)
+print(3.14)
+print(1000)
+print(5+3)
+print(5*3)
+print(5/3)
+
+print('풍선')
+print("string")
+
+print('z'*8)
+
+
+# 참 / 거짓
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+
+# 애완동물을 소개
+
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+print("우리집 "+ animal + "의 이름은 " + name + "에요")
+#print(name +"는 "+ str(age)+"살이며, "+ hobby + "을 아주 좋아해요")
+print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
+
+'''
+주
+석
+처
+리
+'''
