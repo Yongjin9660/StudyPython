d6451d0c33283ac4384c5719861897999741b0a9
YongjinKim <dydwls0669@gmail.com>
2020-09-08 16:16:01 +0900

    operator

---
+++ operator.py
@@ -0,0 +1,25 @@
+print(1+1)
+print(3-2)
+print(5*2)
+print(6/2)
+
+print(2**3)     # 2^3=8
+print(5%3)      # 나머지
+print(5//3)     # 몫 1
+
+print(10 > 3)
+print(4 >= 7)
+
+print(3==3)
+
+print(1 != 3)       # True
+print(not(1!=3))    # False
+
+print((3>0) and (3<5))  #True
+print((3>0) & (3<5))  #True
+
+print((3 > 0) or (3 > 5))  #True
+print((3 > 0) | (3 > 5))  #True
+
+print(5 > 4 > 3)    # True
+print(5 > 4 > 7)    # False
