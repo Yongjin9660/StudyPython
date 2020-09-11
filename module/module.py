a59ac55dfddb0bce4a281c9ee1a7c6efe1acff23
YongjinKim <dydwls0669@gmail.com>
2020-09-11 15:46:40 +0900

    module

---
+++ module/module.py
@@ -0,0 +1,10 @@
+# import theater_module
+# theater_module.price(3)
+# theater_module.price_morning(4)
+# theater_module.price_soldier(5)
+
+# import theater_module as mv
+# mv.price(3)
+
+from theater_module import *
+price(3)
