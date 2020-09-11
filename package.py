6157bf3829fe76e14aa4ad7c0130bcce677e4618
YongjinKim <dydwls0669@gmail.com>
2020-09-11 16:01:13 +0900

    package

---
+++ package.py
@@ -0,0 +1,15 @@
+# import travel_package.thailand
+# trip_to = travel_package.thailand.ThailandPackage()
+# trip_to.detail()
+
+# from travel_package import vietnam
+# trip_to2 = vietnam.VietnamPackage()
+# trip_to2.detail()
+
+from travel_package import *
+
+trip_to2 = vietnam.VietnamPackage()
+trip_to2.detail()
+
+trip_to = thailand.ThailandPackage()
+trip_to.detail()
